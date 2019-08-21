7b399f4eab193372af0e1d49e146dc3dde5b13f6
Rustam Botirov <rbotirov@intetics.com>
2019-08-21 17:59:25 +0300

    bug fix

--- js/script.js
+++ js/script.js
@@ -1,46 +1,7 @@
-$(document).ready(function() {
-
-    $('.carousel').slick({
-        asNavFor: '.carousel-bg',
-        dots: true
-    });
-    $('.carousel-bg').slick({
-        arrows: false,
-        asNavFor: '.carousel',
-        swipe: false,
-        fade: true
-    });
-
-    $('.posts-big-carousel').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        responsive: [{
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-
-                }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-
-
-        ]
-    });
-    $('.posts-small-carousel').slick({
-        autoplay: true,
-        infinate: true,
-        arrows: false,
-        dots: true
-    });
-    $('.burger').click(function() {
-        $('.nav-container').toggleClass('active');
-        $('.burger').toggleClass('active');
-    });
-});
+document.getElementById("link").onclick = function() {
+    var link = document.getElementById("play-video");
+    var url = document.getElementById("url").value;
+    console.log(typeof url);
+    
+    link.setAttribute("href", url);
+}
