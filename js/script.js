6d67bd24739de38db357a53bf6547febc45c8895
Rustam Botirov <rbotirov@intetics.com>
2019-12-11 13:23:16 +0200

    seve url in local/session storage

--- js/script.js
+++ js/script.js
@@ -1,30 +1,48 @@
-const setUrl = document.querySelector("#set_url");
+const setUrl = document.querySelector("#set_url"),
+    play = document.getElementById("play_video"),
+    urlObj = document.getElementById("url"),
+    localUrl = localStorage.getItem('url');
+sessionUrl = sessionStorage.getItem('url');
+
+if (sessionUrl) {
+    play.href = localUrl;
+    play.href = localUrl;
+    makeVisible(play);
+} else if (localUrl) {
+    play.href = localUrl;
+    play.href = localUrl;
+    makeVisible(play);
+}
+
 setUrl.addEventListener('click', setUrlHandler);
 
 function setUrlHandler(e) {
     e.preventDefault();
-    const play = document.getElementById("play_video"),
-        urlObj = document.getElementById("url");
     url = urlObj.value;
     if (!url) {
         alert('Please set URL!');
         return;
     }
-    play.setAttribute("href", url);
-    const enteredUrl = play.getAttribute('href'),
+    play.href = url;
+    const enteredUrl = play.href,
         checkedUrl = isValidUrl(enteredUrl);
     if (checkedUrl) {
-        play.classList.add('visible');
+        makeVisible(play);
         setUrl.classList.add('success');
-        urlObj.value = '';
+        urlObj.value = ''
+        localStorage.setItem('url', url);
+        sessionStorage.setItem('url', url);
     } else {
         alert(`"${enteredUrl}" - is not valid URL. Please set valid URL!`);
         play.classList.remove('visible');
         setUrl.classList.remove('success');
-
     }
 }
 
 function isValidUrl(url) {
     return url.toLowerCase().includes('youtube');
 };
+
+function makeVisible(play) {
+    play.classList.add('visible');
+}
